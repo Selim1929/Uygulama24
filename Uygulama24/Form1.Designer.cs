d6dd168ac87e9179b35b6881eda9a256df0a0ca2
Selim Gül <selimglselimm@gmail.com>
2024-03-21 14:08:29 +0300

    Proje dosyası ekle.

---
+++ Uygulama24/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Uygulama24
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtDersNotu = new System.Windows.Forms.TextBox();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtNumara = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gridListe = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridListe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSil);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.txtDersNotu);
+            this.groupBox1.Controls.Add(this.txtAdSoyad);
+            this.groupBox1.Controls.Add(this.txtNumara);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(22, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 210);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Öğrenci Bilgileri";
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(25, 145);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 7;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(133, 145);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 6;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtDersNotu
+            // 
+            this.txtDersNotu.Location = new System.Drawing.Point(133, 103);
+            this.txtDersNotu.Name = "txtDersNotu";
+            this.txtDersNotu.Size = new System.Drawing.Size(100, 20);
+            this.txtDersNotu.TabIndex = 5;
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.AcceptsReturn = true;
+            this.txtAdSoyad.Location = new System.Drawing.Point(133, 63);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyad.TabIndex = 4;
+            // 
+            // txtNumara
+            // 
+            this.txtNumara.Location = new System.Drawing.Point(133, 27);
+            this.txtNumara.Name = "txtNumara";
+            this.txtNumara.Size = new System.Drawing.Size(100, 20);
+            this.txtNumara.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(9, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ders Notu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(7, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AdSoyad";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(7, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numara";
+            // 
+            // gridListe
+            // 
+            this.gridListe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridListe.Location = new System.Drawing.Point(285, 12);
+            this.gridListe.Name = "gridListe";
+            this.gridListe.Size = new System.Drawing.Size(468, 264);
+            this.gridListe.TabIndex = 1;
+            this.gridListe.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridListe_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gridListe);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridListe)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDersNotu;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtNumara;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.DataGridView gridListe;
+        private System.Windows.Forms.Button btnSil;
+    }
+}
+
---
+++ Uygulama24/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama24
+{
+    public partial class Form1 : Form
+    {
+        List<Ogrenciler> liste = new List<Ogrenciler>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Ogrenciler ogrenciler = new Ogrenciler();
+            ogrenciler.AdSoyad=txtAdSoyad.Text;
+            ogrenciler.Numara=int.Parse(txtNumara.Text);
+            ogrenciler.DersNotu=int.Parse(txtDersNotu.Text);
+            liste.Add(ogrenciler);
+            Bagla();
+        }
+        private void Bagla()
+        {
+            gridListe.DataSource = null;
+            gridListe.DataSource = liste;
+        }
+
+        private void gridListe_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            {
+                txtNumara.Text = gridListe.CurrentRow.Cells[0].Value.ToString();
+                txtAdSoyad.Text = gridListe.CurrentRow.Cells[1].Value.ToString();
+                txtDersNotu.Text = gridListe.CurrentRow.Cells[2].Value.ToString();
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            {
+                // DataGridView'de seçili bir satırın olup olmadığını kontrol et
+                if (gridListe.SelectedRows.Count > 0)
+                {
+                    // Seçili satırın indeksini al
+                    int selectedIndex = gridListe.SelectedRows[0].Index;
+
+                    // DataGridView'deki veri kaynağını al
+                    var dataSource = gridListe.DataSource;
+
+                    // Veri kaynağı bir BindingSource ise
+                    if (dataSource is BindingSource bindingSource)
+                    {
+                        // Veri kaynağının List özelliğini kontrol et
+                        if (bindingSource.List is IList list)
+                        {
+                            // Seçili satırı veri kaynağından kaldır
+                            list.RemoveAt(selectedIndex);
+
+                            // DataGridView'i güncelle
+                            gridListe.DataSource = null;
+                            gridListe.DataSource = bindingSource;
+                        }
+                    }
+                    else if (dataSource is IList list)
+                    {
+                        // Seçili satırı veri kaynağından kaldır
+                        list.RemoveAt(selectedIndex);
+
+                        // DataGridView'i güncelle
+                        gridListe.DataSource = null;
+                       gridListe.DataSource = list;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Veri kaynağı doğru formatta değil!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen bir satır seçin!");
+                }
+            }
+        }   
+    }
+    class Ogrenciler
+    {
+        public int Numara { get; set; }
+        public string AdSoyad { get; set; }
+        public int DersNotu { get; set; }
+    }
+}
